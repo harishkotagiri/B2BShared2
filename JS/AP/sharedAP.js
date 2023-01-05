46bfb5298e2cf700e1bb708cbb8ff16aec6b2074
Kotagiri <Hareesh.Kotagiri@pepsico.com>
2023-01-05 19:29:08 +0530

    adding test case framework

---
+++ JS/AP/sharedAP.js
@@ -0,0 +1,44 @@
+const getTaxData = (reqObj) => {
+    const tempObj = (typeof reqObj !== 'object') ? JSON.parse(reqObj) : reqObj
+    const val = tempObj.num1 + tempObj.num2 + 50;    
+    return {tax: val};
+}
+
+const getTotalData = () => {
+    const tempObj = (typeof reqObj !== 'object') ? JSON.parse(reqObj) : reqObj
+    const val = tempObj.num1 + tempObj.num2 + 20;    
+    return {total: val};
+}
+
+function getTaxOffers(rObj) {
+    sendToMobile(getTaxData(rObj));
+}
+
+function getToal(rObj) {
+    sendToMobile(getTotalData(rObj));
+}
+
+// function getOffers(reqObj) {
+//     let tempObj = reqObj;
+//     if (typeof tempObj !== 'object') {
+//         tempObj = JSON.parse(reqObj)
+//     }
+//     let val = parseInt(tempObj.num1) + parseInt(tempObj.num2) + 50;
+//     val = val ? val : 'error in external JS';
+//     let data = {type: 'Response from External JS:', val};
+//     sendToMobile(data);
+// }
+
+function sendToMobile(data) {
+  window.ReactNativeWebView.postMessage(JSON.stringify(data));
+}
+
+ // For Web
+
+window.calculateTax = (val1, val2) => {
+    return  val1 + val2 + 20;
+}
+
+window.calculateVals = (val1, val2) => {
+    return val1 + val2;
+}
